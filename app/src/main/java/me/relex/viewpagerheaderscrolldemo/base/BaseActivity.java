22b6e4ee67a76a77d1861e46d6623284c7773546
kevin <agonyice0115@gmail.com>
2017-09-01 11:12:08 +0800

    clear code

--- app/src/main/java/me/relex/viewpagerheaderscrolldemo/base/BaseActivity.java
+++ app/src/main/java/me/relex/viewpagerheaderscrolldemo/base/BaseActivity.java
@@ -16,60 +16,64 @@ import me.relex.viewpagerheaderscrolldemo.utils.ActivityManager;
  */
 public class BaseActivity extends AppCompatActivity {
 
-  private LinearLayout rootLayout;
-  private ActivityManager mActivityManager = ActivityManager.getActivityManager(this);
+    private LinearLayout rootLayout;
+    private ActivityManager mActivityManager = ActivityManager.getActivityManager(this);
 
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    // 这句很关键，注意是调用父类的方法
-    super.setContentView(R.layout.activity_base);
-    // 经测试在代码里直接声明透明状态栏更有效
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-      WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
-      localLayoutParams.flags =
-          (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 这句很关键，注意是调用父类的方法
+        super.setContentView(R.layout.activity_base);
+        // 经测试在代码里直接声明透明状态栏更有效
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+            localLayoutParams.flags =
+                    (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+        }
+        initToolbar();
+        mActivityManager.putActivity(this);
+
     }
-    initToolbar();
-    mActivityManager.putActivity(this);
-
-  }
 
 
-  private void initToolbar() {
+    private void initToolbar() {
 
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
-  }
-
-
-  @Override
-  public void setContentView(int layoutId) {
-
-    setContentView(View.inflate(this, layoutId, null));
-  }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
 
 
-  @Override
-  public void setContentView(View view) {
+    @Override
+    public void setContentView(int layoutId) {
 
-    rootLayout = (LinearLayout) findViewById(R.id.root_layout);
-    if (rootLayout == null) return;
-    rootLayout.addView(view, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-        ViewGroup.LayoutParams.MATCH_PARENT));
-    initToolbar();
-  }
-  @Override protected void onDestroy() {
-    super.onDestroy();
-    mActivityManager.removeActivity(this);
-  }
-  public void exit() {
-    mActivityManager.exit();
-  }
+        setContentView(View.inflate(this, layoutId, null));
+    }
 
 
-  @Override protected void onSaveInstanceState(Bundle outState) {
-    // super.onSaveInstanceState(outState); //将这一行注释掉，阻止activity保存fragment的状态
-  }
+    @Override
+    public void setContentView(View view) {
+
+        rootLayout = (LinearLayout) findViewById(R.id.root_layout);
+        if (rootLayout == null) return;
+        rootLayout.addView(view, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+        initToolbar();
+    }
+
+
+    @Override protected void onDestroy() {
+        super.onDestroy();
+        mActivityManager.removeActivity(this);
+    }
+
+
+    public void exit() {
+        mActivityManager.exit();
+    }
+
+
+    @Override protected void onSaveInstanceState(Bundle outState) {
+        // super.onSaveInstanceState(outState); //将这一行注释掉，阻止activity保存fragment的状态
+    }
 }
